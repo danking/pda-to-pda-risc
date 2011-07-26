5030c1c630b30c912bb14977c735af49507ce542
Dan King <danking@ccs.neu.edu>
2011-07-26 17:04:58 -0400

    create `symbol-append', add a NAME-body state, update formatting of gotos to point to NAME-body, add a bunch of grammars which form the data definitions

--- pda-to-pda0.rkt
+++ pda-to-pda0.rkt
@@ -71,21 +71,93 @@
 ;; expands a state (name, goto actions, other actions) into its primary state
 ;; and its reduction state
 (define (produce-state-block name gotos other-actions accept-block)
-  (let ((reduce-state (string->symbol
-                       (string-append (symbol->string name)
-                                      "-reduce"))))
+  (let ((body-state   (symbol-append name '-body))
+        (reduce-state (symbol-append name '-reduce)))
     `((block ,name #f #f
+             push-token
+             (jump ,body-state))
+      (block ,body-state #f #f
              (push-state ,reduce-state)
-             (lcase . ,(format-case-clauses other-actions accept-block)))
+             (lcase . ,(format-non-goto-actions other-actions accept-block)))
       (block ,reduce-state #f #f
              (push-state ,reduce-state)
-             (gcase . ,(format-case-clauses gotos accept-block))))))
+             (gcase . ,(format-goto-actions gotos accept-block))))))
+
+;; Symbol Symbol -> Symbol
+;; appends the string versions of the symbols to produce a new symbol
+;; (symbol-append 'foo 'bar) => 'foobar
+(define (symbol-append a b)
+  (string->symbol (string-append (symbol->string a)
+                                 (symbol->string b))))
 
 ;; trims off the clause type identifier and contorts accept clauses so that
 ;; they point to the accepting block
-(define (format-case-clauses clauses accept-block)
+(define (format-non-goto-actions clauses accept-block)
   (map (lambda (x)
          (if (eq? (first x) 'accept)
              `(,(second x) ,accept-block)
              `(,(second x) ,(third x))))
        clauses))
+
+;; similar to `format-non-goto-actions', but this changes the target states to
+;; NAME-body to avoid a token push on the NAME state
+(define (format-goto-actions clauses accept-block)
+  (map (lambda (x)
+         `(,(second x) ,(symbol-append (third x) '-body)))
+       clauses))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Grammars
+
+;; pda ::= (pda-clause ...)
+;; pda-clause ::= (COMMENT whatever ...)
+;;        | (TOKENS token ...)
+;;              | (ERROR-SYMBOL ident [semantic-value-proc])
+;;              | (NO-SKIP token ...)
+;;              | (STATE state-name action ...)
+;;              | (RULE rule-name non-term bindings semantic-action)
+
+;; action ::= (COMMENT whatever ...)
+;;          | (SHIFT  lookahead state-name)
+;;          | (REDUCE lookahead rule-name)
+;;          | (ACCEPT lookahead)
+;;          | (GOTO non-term state-name)
+;;    | (ERROR-SHIFT ident state-name)
+
+;; lookahead ::= (token ... [#f])  ; Terminating #f means eos.
+;; bindings ::= (boolean ...)
+
+;; state-name, rule-name ::= ident
+;; token, non-term ::= ident
+
+
+;; pda0 ::= (clause ...)
+
+;; clause ::= (TOKENS token ...)
+;;          | (NO-SKIP token ...)
+;;          | (BLOCK block-name error-handler token-count  insn ... branch)
+;;          | (RULE rule-name non-term bindings semantic-action)
+;;          | (COMMENT stuff ...)
+
+;; insn ::= PUSH-TOKEN | EAT-TOKEN
+;;        | (REDUCE rule-name) | (REDUCE0 rule-name)
+;;        | (POP-STATES integer)
+;;        | (PUSH-STATE block-name)
+;;        | (COMMENT stuff ...)
+
+;; branch ::= (LCASE (lookahead block-name) ...)
+;;          | (GCASE (non-term block-name) ...)
+;;          | (SKIP-UNTIL (lookahead block-name) ...)
+;;          | ACCEPT
+;;          | RETURN
+;;          | (JUMP block-name)
+
+;; bindings ::= (boolean ...)
+;; lookahead ::= (token ... [#f])  ; Terminating #f means eos.
+
+;; error-handler ::= block-name | #f
+;; token-count ::= integer | #f
+
+;; block-name, rule-name ::= ident
+;; token, non-term ::= ident
