dc366151a9a6ef1399615023338b346f4a55adbd
Dan King <danking@ccs.neu.edu>
2011-07-27 10:14:49 -0400

    changed an effectful `map' to a `foreach', reindented

--- tests/pda-to-pda0.rkt
+++ tests/pda-to-pda0.rkt
@@ -13,73 +13,73 @@
               pda2-risc)
 
 ;; convert-rule
-(map (lambda (in expected)
-       (check-equal? (convert-rule in 'accept-block) expected))
-     '((rule r1 start (#f #f)    1)
-       (rule r2 start (#f #f #f) 2)
-       (rule r3 foo   (#f)       3)
-       (rule r4 foo   (#f #f)    4))
-     '((block r1 #f #f
-              (reduce r1)
-              (pop-states 2)
-              return)
-       (block r2 #f #f
-              (reduce r2)
-              (pop-states 3)
-              return)
-       (block r3 #f #f
-              (reduce r3)
-              (pop-states 1)
-              return)
-       (block r4 #f #f
-              (reduce r4)
-              (pop-states 2)
-              return)))
+(for-each (lambda (in expected)
+            (check-equal? (convert-rule in 'accept-block) expected))
+          '((rule r1 start (#f #f)    1)
+            (rule r2 start (#f #f #f) 2)
+            (rule r3 foo   (#f)       3)
+            (rule r4 foo   (#f #f)    4))
+          '((block r1 #f #f
+                   (reduce r1)
+                   (pop-states 2)
+                   return)
+            (block r2 #f #f
+                   (reduce r2)
+                   (pop-states 3)
+                   return)
+            (block r3 #f #f
+                   (reduce r3)
+                   (pop-states 1)
+                   return)
+            (block r4 #f #f
+                   (reduce r4)
+                   (pop-states 2)
+                   return)))
 
 ;; convert-state
-(map (lambda (in expected)
-       (let ((result (convert-state in 'accept-block)))
-         (check-equal? result expected)))
-     '((state s1
-              (shift (A) s2)
-              (goto start s8)
-              (goto foo s3))
-       (state s2
-              (reduce () r3))
-       (state s3
-              (shift (A) s4)
-              (shift (C) s5)
-              (shift (B) s6)))
-     '(((block s1 #f #f
-               push-token
-               (jump s1-body))
-        (block s1-body #f #f
-               (push-state s1-reduce)
-               (lcase ((A) s2)))
-        (block s1-reduce #f #f
-               (push-state s1-reduce)
-               (gcase (start s8-body)
-                      (foo s3-body))))
-       ((block s2 #f #f
-               push-token
-               (jump s2-body))
-        (block s2-body #f #f
-               (push-state s2-reduce)
-               (lcase (() r3)))
-        (block s2-reduce #f #f
-               (push-state s2-reduce)
-               (gcase)))
-       ((block s3 #f #f
-               push-token
-               (jump s3-body))
-        (block s3-body #f #f
-               (push-state s3-reduce)
-               (lcase ((A) s4)
-                      ((C) s5)
-                      ((B) s6)))
-        (block s3-reduce #f #f
-               (push-state s3-reduce)
-               (gcase)))))
+(for-each (lambda (in expected)
+            (let ((result (convert-state in 'accept-block)))
+              (check-equal? result expected)))
+          '((state s1
+                   (shift (A) s2)
+                   (goto start s8)
+                   (goto foo s3))
+            (state s2
+                   (reduce () r3))
+            (state s3
+                   (shift (A) s4)
+                   (shift (C) s5)
+                   (shift (B) s6)))
+          '(((block s1 #f #f
+                    push-token
+                    (jump s1-body))
+             (block s1-body #f #f
+                    (push-state s1-reduce)
+                    (lcase ((A) s2)))
+             (block s1-reduce #f #f
+                    (push-state s1-reduce)
+                    (gcase (start s8-body)
+                           (foo s3-body))))
+            ((block s2 #f #f
+                    push-token
+                    (jump s2-body))
+             (block s2-body #f #f
+                    (push-state s2-reduce)
+                    (lcase (() r3)))
+             (block s2-reduce #f #f
+                    (push-state s2-reduce)
+                    (gcase)))
+            ((block s3 #f #f
+                    push-token
+                    (jump s3-body))
+             (block s3-body #f #f
+                    (push-state s3-reduce)
+                    (lcase ((A) s4)
+                           ((C) s5)
+                           ((B) s6)))
+             (block s3-reduce #f #f
+                    (push-state s3-reduce)
+                    (gcase)))))
 
 ;; categorize-actions
 (let-values (((gotos actions) (categorize-actions '((shift (A) s2)
