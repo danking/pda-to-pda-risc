26c9ddc428c106bfe42ba74afd766bb2a0ca6237
Daniel King <danking@ccs.neu.edu>
2011-08-05 14:38:05 -0400

    major overhaul of pdarisc tests, new examples
    The pdarisc tests were completely rewritten. Everything now passes and actually
    describes the intended behavior of the pdarisc convertor. In doing so,
    I created new example pdas for use with the pda-risc tests

--- tests/pda-to-pdarisc.rkt
+++ tests/pda-to-pdarisc.rkt
@@ -1,24 +1,51 @@
 #lang racket
 (require rackunit
-         #;"example-pdas.rkt")
+         "example-risc-pdas.rkt")
 
 (require/expose "../pda-to-pdarisc.rkt"
-                (convert-pda convert-rule convert-state categorize-actions))
+                (convert-pda convert-rule convert-state segregate-gotos
+                 segregate-eos get-start-state get-eos-token))
 
-(define pda1
-  '((tokens A B $eos)
-    (state s1 (shift A s2) (goto start s6))
-    (state s2 (shift A s2) (shift B s3) (goto start s4))
-    (state s3 (reduce #t r2))
-    (state s4 (shift B s5))
-    (state s5 (reduce #t r1))
-    (state s6 (accept $eos))
-    (rule r1 start  (#f #t #f) (lambda (x) (+ 2 x)))
-    (rule r2 start  (#f #f)    (lambda () 2))
-    (rule r3 accept (#t #f)    (lambda (x) x))))
+;; GET-START-TOKEN and GET-EOS-TOKEN
+(check-equal? (get-start-state pda1) 's1)
+(check-exn exn:fail?
+           (lambda () (get-start-state (cdddr pda1))))
+(check-equal? (get-eos-token pda1) '$eos)
+(check-exn exn:fail?
+           (lambda () (get-eos-token (cddr pda1))))
 
-(convert-pda pda1)
+;; CATEGORIZE-GOTOS
+(let-values (((gotos others) (segregate-gotos (cddr multiple-shift-state))))
+  (check-equal? gotos '((goto start s4)))
+  (check-equal? others '((shift (A) s2) (shift (B) s3))))
+(let-values (((gotos others) (segregate-gotos '((shift (F) foo)
+                                                (reduce (B) r1)
+                                                (reduce ($eos) r3)))))
+  (check-equal? gotos '())
+  (check-equal? others '((shift (F) foo)
+                         (reduce (B) r1)
+                         (reduce ($eos) r3))))
 
+;; CATEGORIZE-EOS
+(let-values (((eos others) (segregate-eos (cddr accepting-state) '$eos)))
+  (check-equal? eos '((accept ($eos))))
+  (check-equal? others '()))
+(let-values (((eos others) (segregate-eos '((shift (F) foo)
+                                            (reduce (B) r1)
+                                            (reduce ($eos) r3))
+                                          '$eos)))
+  (check-equal? eos '((reduce ($eos) r3)))
+  (check-equal? others '((shift (F) foo) (reduce (B) r1))))
+
+;; CONVERT-STATE
+(for-each (lambda (in expected)
+            (check-equal? (convert-state in '$eos) expected))
+          (list single-shift-state multiple-shift-state
+                reducing-state accepting-state)
+          (list single-shift-state-risc multiple-shift-state-risc
+                reducing-state-risc accepting-state-risc))
+
+;; CONVERT-RULE
 (for-each (lambda (in expected)
             (check-equal? (convert-rule in) expected))
           '((rule r1 start (#f #t #f) (lambda (x) (+ 2 x)))
