87a159667609b648cc8da7ae94ad57ca2156365e
Daniel King <danking@ccs.neu.edu>
2011-08-23 09:48:15 -0400

    use ustruct's, better documentation, and action structure hierarchy

--- pda-data.rkt
+++ pda-data.rkt
@@ -3,88 +3,60 @@
 (require (rename-in racket
                     (define-struct define-struct*)))
 
-(define-syntax define-struct
+(define-syntax symbol-append
   (syntax-rules ()
-    ((_ name fields)
-     (define-struct* name fields #:transparent))))
+    [(_ sym ...)
+     (string->symbol (string-append (symbol->string sym) ...))]))
 
-;; a PDAState is a (make-state Symbol
-;;                             [ListOf (U ShiftAction
-;;                                        ReduceAction
-;;                                        AcceptAction)]
-;;                             GotoAction)
-(define-struct state (name non-gotos gotos))
+(define-syntax (define-ustruct stx)
+  (define (make-updater-name struct-name field-name)
+    (symbol-append struct-name '-update- field-name))
+  (define (make-updater-proc name-stx field-stx)
+    (let* ((field-name (syntax->datum field-stx))
+           (struct-name (syntax->datum name-stx))
+           (update-proc (datum->syntax name-stx
+                                       (make-updater-name struct-name
+                                                          field-name))))
+      #`(define (#,update-proc val s)
+          (struct-copy #,name-stx s [#,field-stx val]))))
 
-(define-struct action (lookahead target))
-(define-struct (shift action) ())
-(define-struct (reduce action) ())
-(define-struct (goto action) ())
-(define-struct (accept action) ())
+  (syntax-case stx ()
+      ((_ name fields kws ...)
+       #`(begin (define-struct name fields kws ...)
+                #,@(syntax-map (lambda (f)
+                                 (make-updater-proc #'name f))
+                               #'fields)))))
 
-;; an [Action X] is a (list X lookahead target)
-;; where lookahead is a Symbol, and
-;;       target is a Symbol
 
-;; a ShiftAction is an [Action 'shift]
+;; A PDA is a (make-pda [ListOf Symbol]
+;;                      [Maybe Symbol]
+;;                      [Maybe Symbol]
+;;                      [ListOf State]
+;;                      [ListOf Rule])
+(define-ustruct pda (tokens eos start states rules))
 
-;; a ReduceAction is an [Action 'reduce]
+(define empty-pda (make-pda '() #f #f '() '()))
 
-;; a GotoAction is an [Action 'goto]
+;; a State is a (make-state Symbol
+;;                          [ListOf (U Shift
+;;                                     Reduce
+;;                                     Accept)]
+;;                          Goto)
+(define-ustruct state (name non-gotos gotos))
 
-;; an AcceptAction is a (list 'accept lookahead)
-;; where lookahead is a Symbol
+;; a Rule is a (make-rule Symbol
+;;                        Symbol
+;;                        [ListOf [Maybe Symbol]]
+;;                        SExp)
+(define-ustruct rule (name nt bindings sem-act))
 
-;; StateAction is (U ShiftAction ReduceAction GotoAction AcceptAction)
 
-(define-struct pda (tokens eos start states rules reducible-states))
-(define-struct rule (name nt bindings sem-act))
+;; an Action is a (make-action Symbol)
+(define-ustruct action (lookahead))
+;; a TAction is a (make-taction Symbol Symbol)
+(define-ustruct (taction action) (target))
 
-(define empty-pda (make-pda '() #f #f '() '() (hasheq)))
-
-(define (pda-set-reducible-states hsh pda)
-  (make-pda (pda-tokens pda)
-            (pda-eos pda)
-            (pda-start pda)
-            (pda-states pda)
-            (pda-rules pda)
-            hsh))
-
-(define (pda-set-tokens tokens pda)
-  (make-pda tokens
-            (pda-eos pda)
-            (pda-start pda)
-            (pda-states pda)
-            (pda-rules pda)
-            (pda-reducible-states pda)))
-
-(define (pda-set-eos token pda)
-  (make-pda (pda-tokens pda)
-            token
-            (pda-start pda)
-            (pda-states pda)
-            (pda-rules pda)
-            (pda-reducible-states pda)))
-
-(define (pda-set-start state pda)
-  (make-pda (pda-tokens pda)
-            (pda-eos pda)
-            state
-            (pda-states pda)
-            (pda-rules pda)
-            (pda-reducible-states pda)))
-
-(define (pda-add-state state pda)
-  (make-pda (pda-tokens pda)
-            (pda-eos pda)
-            (pda-start pda)
-            (cons state (pda-states pda))
-            (pda-rules pda)
-            (pda-reducible-states pda)))
-
-(define (pda-add-rule rule pda)
-  (make-pda (pda-tokens pda)
-            (pda-eos pda)
-            (pda-start pda)
-            (pda-states pda)
-            (cons rule (pda-rules pda))
-            (pda-reducible-states pda)))
+(define-ustruct (shift   taction) ())
+(define-ustruct (reduce  taction) ())
+(define-ustruct (goto    taction) ())
+(define-ustruct (accept  action) ())
