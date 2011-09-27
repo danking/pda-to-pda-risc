a791d4c2114b57d1afaeacab0cec0a529c24785a
Daniel King <danking@ccs.neu.edu>
2011-09-27 14:56:50 -0400

    Move DEFINE-USTRUCT to a separate file

--- pda-data.rkt
+++ pda-data.rkt
@@ -1,32 +1,6 @@
 #lang racket
 (provide (all-defined-out))
-(require (rename-in racket
-                    (define-struct define-struct*)))
-
-(define-syntax symbol-append
-  (syntax-rules ()
-    [(_ sym ...)
-     (string->symbol (string-append (symbol->string sym) ...))]))
-
-(define-syntax (define-ustruct stx)
-  (define (make-updater-name struct-name field-name)
-    (symbol-append struct-name '-update- field-name))
-  (define (make-updater-proc name-stx field-stx)
-    (let* ((field-name (syntax->datum field-stx))
-           (struct-name (syntax->datum name-stx))
-           (update-proc (datum->syntax name-stx
-                                       (make-updater-name struct-name
-                                                          field-name))))
-      #`(define (#,update-proc val s)
-          (struct-copy #,name-stx s [#,field-stx val]))))
-
-  (syntax-case stx ()
-      ((_ name fields kws ...)
-       #`(begin (define-struct name fields kws ...)
-                #,@(syntax-map (lambda (f)
-                                 (make-updater-proc #'name f))
-                               #'fields)))))
-
+(require "define-ustruct.rkt")
 
 ;; A PDA is a (make-pda [ListOf Symbol]
 ;;                      [Maybe Symbol]
