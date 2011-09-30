50f0217f594d7b82ec98d87e008371e7ea7a1c1a
Daniel King <danking@ccs.neu.edu>
2011-09-30 11:02:18 -0400

    Add a compiler for the PDA language that targets the PDA-RISC language

---
+++ compile-pda.rkt
@@ -0,0 +1,135 @@
+#lang racket
+(require "pda-data.rkt"
+         (rename-in "pdarisc-data.rkt"
+                    [struct:state struct:risc-state]
+                    [state? risc-state?]
+                    [state risc-state]
+                    [make-state make-risc-state]
+                    [state-id risc-state-id]
+                    [struct:accept struct:risc-accept]
+                    [accept? risc-accept?]
+                    [accept risc-accept]
+                    [make-accept make-risc-accept]
+                    [accept-vals risc-accept-vals])
+         "parse-pda.rkt"
+         rackunit)
+(provide compile-pda)
+
+(define make-dict hasheq)
+;; a Reduce-To-Table is a [Dict Non-Terminal [Dict State-Name State-Name]]
+
+
+;; compile-pda : PDA -> PDA-RISC
+(define (compile-pda a-pda)
+  (match a-pda
+    ((pda tokens eos start states rules)
+     (let ((rto-table (gather-rto-table states)))
+       (make-pdarisc
+        (list (make-label
+               (append (map state-name states)
+                       (map rule-name  rules))
+               (append (map state-stack-type states)
+                       (map rule-stack-type  rules))
+               (append (map (lambda (x) #f) states)
+                       (map (lambda (x) #f) rules))
+               (append (map (lambda (x) '()) states)
+                       (map (lambda (x) '()) rules))
+               (append (map (lambda (x) (list (compile-state x)))
+                            states)
+                       (map (lambda (x) (list (compile-rule x rto-table)))
+                            rules))
+               (list (make-go start '())))))))))
+
+;; gather-rto-table : [ListOf State] -> Reduce-To-Table
+(define (gather-rto-table states)
+  (foldl (lambda (gotos s table)
+           (foldl (lambda (g t)
+                    (add-reduce-to-pair g s t))
+                  table
+                  gotos))
+         (make-dict)
+         (map state-gotos states)
+         (map state-name states)))
+
+;; add-reduce-to-pair : Goto Symbol Reduce-To-Table -> Reduce-To-Table
+;; reduce-to-pair will add the implicit Reduce-To-Table entry from the goto
+(define (add-reduce-to-pair a-goto curr-st table)
+  (match a-goto
+    ((goto nt dest-st)
+     (dict-update table nt (lambda (st-map)
+                             (dict-set st-map curr-st dest-st))
+                  (make-dict)))))
+
+;; compile-state : State -> Insn*
+(define (compile-state st)
+  (match st
+    ((state name stype token-actions gotos)
+     (make-block*
+      (list
+       (make-if-eos
+        (accept '(reject))
+        (make-block*
+         (list
+          (make-get-token)
+          (make-token-case
+           (map (lambda (x) (first (action-lookahead x)))
+                token-actions)
+           (map (lambda (x) (compile-action x name))
+                token-actions))))))))))
+
+
+;; compile-rule : Rule Reduce-To-Table -> Insn*
+(define (compile-rule r rto-table)
+  (match r
+    ((rule name stype nt args sem-act)
+     (let ((rto-states (dict-ref rto-table nt)))
+       (make-block*
+        `(,@(compile-rule-args args)
+          ,(make-sem-act (filter (lambda (x) x) args) '(ret-val) sem-act)
+          ,(make-push (make-var-ref 'target))
+          ,(make-push (make-var-ref 'ret-val))
+          ,(make-state-case (make-var-ref 'target)
+                            (dict-keys rto-states)
+                            (map (lambda (target) (list (make-go target '())))
+                                 (dict-values rto-states)))))))))
+
+
+;; compile-rule-args : [ListOf Symbol] -> [ListOf Insn]
+(define (compile-rule-args args)
+  (if (empty? args)
+      (list (make-assign 'v (make-pop))
+            (make-assign 'target (make-pop))
+            (make-push (make-var-ref 'v))
+            (make-push (make-var-ref 'target)))
+      (foldl (lambda (x xs)
+               (if x
+                   (list* (make-assign x (make-pop))
+                          (make-assign '_ (make-pop))
+                          xs)
+                   (list* (make-assign '_ (make-pop))
+                          (make-assign '_ (make-pop))
+                          xs)))
+             (list (make-assign (first args) (make-pop))
+                   (make-assign 'target (make-pop)))
+             (rest args))))
+
+
+;; compile-action : Action Symbol -> Insn*
+(define (compile-action a curr-state)
+  (match a
+    ((shift l st)
+     (make-block* (list (make-push (make-risc-state curr-state))
+                        (make-push (make-curr-token 0))
+                        (make-drop-token)
+                        (make-go st '()))))
+    ((reduce l st)
+     (make-block* (list (make-drop-token)
+                        (make-go st '()))))
+    ((goto nt st)
+     (make-block* (list (make-push (make-risc-state curr-state))
+                        (make-push (make-nterm nt))
+                        (make-go st '()))))
+    ((accept l)
+     (make-block* (list (make-accept '()))))))
+
+
---
+++ tests/compile-pda.rkt
@@ -0,0 +1,240 @@
+#lang racket
+(require "../pda-data.rkt"
+         (rename-in "../pdarisc-data.rkt"
+                    [struct:state struct:risc-state]
+                    [state? risc-state?]
+                    [state risc-state]
+                    [make-state make-risc-state]
+                    [state-id risc-state-id]
+                    [struct:accept struct:risc-accept]
+                    [accept? risc-accept?]
+                    [accept risc-accept]
+                    [make-accept make-risc-accept]
+                    [accept-vals risc-accept-vals])
+         rackunit)
+(require/expose "../compile-pda.rkt"
+                (compile-pda gather-rto-table
+                             add-reduce-to-pair
+                             compile-state
+                             compile-rule
+                             compile-rule-args
+                             compile-action
+                             make-dict))
+
+(check-equal?
+ (compile-pda
+  (make-pda '(A B plus $eos)
+            '$eos
+            's0
+            (list (make-state 's0
+                              '(())
+                              (list (make-shift '(A) 's1)
+                                    (make-reduce '(B) 'r1))
+                              (list (make-goto 'nt 's1)))
+                  (make-state 's1
+                              '(())
+                              (list (make-shift '(A) 's1)
+                                    (make-reduce '(B) 'r1))
+                              (list (make-goto 'nt 's0))))
+            (list (make-rule 'r1 '(s : ((A s)))
+                             'nt
+                             '(v)
+                             'v))))
+ (make-pdarisc
+  (list (make-label
+         '(s0 s1 r1)
+         '((()) (()) (s : ((A s)))) ; stacks
+         '(#f #f #f) ; token-regs
+         '(() () ()) ; arg-lists
+         (list (list (make-block*
+                      (list
+                       (make-if-eos
+                        (accept '(reject))
+                        (make-block*
+                         (list
+                          (make-get-token)
+                          (make-token-case
+                           '(A B)
+                           (list
+                            (make-block*
+                             (list (make-push (make-risc-state 's0))
+                                   (make-push (make-curr-token 0))
+                                   (make-drop-token)
+                                   (make-go 's1 '())))
+                            (make-block*
+                             (list (make-drop-token)
+                                   (make-go 'r1'())))))))))))
+               (list (make-block*
+                      (list
+                       (make-if-eos
+                        (accept '(reject))
+                        (make-block*
+                         (list
+                          (make-get-token)
+                          (make-token-case
+                           '(A B)
+                           (list
+                            (make-block*
+                             (list (make-push (make-risc-state 's1))
+                                   (make-push (make-curr-token 0))
+                                   (make-drop-token)
+                                   (make-go 's1 '())))
+                            (make-block*
+                             (list (make-drop-token)
+                                   (make-go 'r1 '())))))))))))
+               (list (make-block*
+                      (list
+                       (make-assign 'v (make-pop))
+                       (make-assign 'target (make-pop)) ; state
+                       (make-sem-act '(v) '(ret-val) 'v)
+                       (make-push (make-var-ref 'target))
+                       (make-push (make-var-ref 'ret-val))
+                       (make-state-case (make-var-ref 'target)
+                                        '(s0 s1)
+                                        (list (list (make-go 's1 '()))
+                                              (list (make-go 's0 '()))))))))
+         (list (make-go 's0 '()))))))
+
+
+
+(check-equal? (gather-rto-table
+               (list (make-state 's0
+                                 '(())
+                                 (list (make-shift '(A) 's1)
+                                       (make-reduce '(B) 'r1))
+                                 (list (make-goto 'nt 's1)))
+                     (make-state 's1
+                                 '(())
+                                 (list (make-shift '(A) 's1)
+                                       (make-reduce '(B) 'r1))
+                                 (list (make-goto 'nt 's0)))))
+              (make-dict 'nt (make-dict 's0 's1 's1 's0)))
+
+(check-equal? (gather-rto-table
+               (list (make-state 's0
+                                 '(())
+                                 (list (make-shift '(A) 's1)
+                                       (make-reduce '(B) 'r1))
+                                 (list (make-goto 'nt 's1)))
+                     (make-state 's1
+                                 '(())
+                                 (list (make-shift '(A) 's1)
+                                       (make-reduce '(B) 'r1))
+                                 (list (make-goto 'nt2 's0)))))
+              (make-dict 'nt (make-dict 's0 's1)
+                         'nt2 (make-dict 's1 's0)))
+
+
+(check-equal? (add-reduce-to-pair (make-goto 'nt 's1)
+                                  's0
+                                  (make-dict 'nt (make-dict)))
+              (make-dict 'nt (make-dict 's0 's1)))
+
+(check-equal? (add-reduce-to-pair (make-goto 'nt 's1)
+                                  's0
+                                  (make-dict))
+              (make-dict 'nt (make-dict 's0 's1)))
+
+(check-equal? (add-reduce-to-pair (make-goto 'nt 's0)
+                                  's0
+                                  (make-dict 'nt (make-dict)
+                                             'nt2 (make-dict)))
+              (make-dict 'nt (make-dict 's0 's0)
+                         'nt2 (make-dict)))
+
+(check-equal? (add-reduce-to-pair (make-goto 'nt2 's3)
+                                  's2
+                                  (make-dict 'nt1 (make-dict 's0 's0)
+                                             'nt2 (make-dict)))
+              (make-dict 'nt1 (make-dict 's0 's0)
+                         'nt2 (make-dict 's2 's3)))
+
+(check-equal? (compile-state (make-state 's0
+                                         '(())
+                                         (list (make-shift '(A) 's1)
+                                               (make-reduce '(B) 'r1))
+                                         (list (make-goto 'nt 's1))))
+              (make-block*
+               (list
+                (make-if-eos
+                 (accept '(reject))
+                 (make-block*
+                  (list
+                   (make-get-token)
+                   (make-token-case
+                    '(A B)
+                    (list
+                     (make-block* (list (make-push (make-risc-state 's0))
+                                        (make-push (make-curr-token 0))
+                                        (make-drop-token)
+                                        (make-go 's1 '())))
+                     (make-block* (list (make-drop-token)
+                                        (make-go 'r1 '())))))))))))
+
+(check-equal? (compile-rule (make-rule 'r1
+                                       '((int plus int))
+                                       'nt
+                                       '(v1 #f v2)
+                                       '(+ v1 v2))
+                            (make-dict 'nt (make-dict 's1 's2
+                                                      's2 's0)))
+              (make-block*
+               (list (make-assign 'v2 (make-pop))
+                     (make-assign '_ (make-pop)) ; state
+                     (make-assign '_ (make-pop))
+                     (make-assign '_ (make-pop)) ; state
+                     (make-assign 'v1 (make-pop))
+                     (make-assign 'target (make-pop)) ; state
+                     (make-sem-act '(v1 v2) '(ret-val) '(+ v1 v2))
+                     (make-push (make-var-ref 'target))
+                     (make-push (make-var-ref 'ret-val))
+                     (make-state-case (make-var-ref 'target)
+                                      '(s1 s2)
+                                      (list (list (make-go 's2 '()))
+                                            (list (make-go 's0 '())))))))
+
+(check-equal? (compile-rule (make-rule 'r1 '(s : ((A s))) 'nt '(v) 'v)
+                            (make-dict 'nt (make-dict 's0 's1
+                                                      's1 's0)))
+              (make-block*
+               (list (make-assign 'v (make-pop))
+                     (make-assign 'target (make-pop)) ; state
+                     (make-sem-act '(v) '(ret-val) 'v)
+                     (make-push (make-var-ref 'target))
+                     (make-push (make-var-ref 'ret-val))
+                     (make-state-case (make-var-ref 'target)
+                                      '(s0 s1)
+                                      (list (list (make-go 's1 '()))
+                                            (list (make-go 's0 '())))))))
+
+(check-equal? (compile-rule-args '(v1 #f v2))
+              (list (make-assign 'v2 (make-pop))
+                    (make-assign '_ (make-pop)) ; state
+                    (make-assign '_ (make-pop))
+                    (make-assign '_ (make-pop)) ; state
+                    (make-assign 'v1 (make-pop))
+                    (make-assign 'target (make-pop))))
+
+(check-equal? (compile-rule-args '(v))
+              (list (make-assign 'v (make-pop))
+                    (make-assign 'target (make-pop))))
+
+(check-equal? (compile-rule-args '())
+              (list (make-assign 'v (make-pop))
+                    (make-assign 'target (make-pop))
+                    (make-push (make-var-ref 'v))
+                    (make-push (make-var-ref 'target))))
+(check-equal? (compile-action (make-shift '(A) 's1) 's0)
+              (make-block* (list (make-push (make-risc-state 's0))
+                                 (make-push (make-curr-token 0))
+                                 (make-drop-token)
+                                 (make-go 's1 '()))))
+(check-equal? (compile-action (make-reduce '(B) 'r1) 's0)
+              (make-block* (list (make-drop-token)
+                                 (make-go 'r1 '()))))
+(check-equal? (compile-action (make-goto 'nt 's1) 's0)
+              (make-block* (list (make-push (make-risc-state 's0))
+                                 (make-push (make-nterm 'nt))
+                                 (make-go 's1 '()))))
+(check-equal? (compile-action (make-accept '(A)) 's0)
+              (make-block* (list (make-accept '()))))
