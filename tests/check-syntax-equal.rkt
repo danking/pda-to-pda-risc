3962d2b28ab4cf16a2630d1429742abb91f46435
Daniel King <danking@ccs.neu.edu>
2011-10-25 15:05:17 -0400

    Move `datum-equal?' to check-syntax-equal.rkt and rename
    Now any test file can use the `check-syntax-equal?' macro to access a syntax
    agnostic `check-equal?'. Specifically, this procedure will declare any two
    pieces of syntax which have the same datum form, to be the same.

---
+++ tests/check-syntax-equal.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(require rackunit)
+(provide check-syntax-equal?)
+
+;; don't pass infinite sequences to this.
+(define (syntax-equal? x y)
+  (let-values (((x y) (if (and (syntax? x) (syntax? y))
+                          (values (syntax->datum x) (syntax->datum y))
+                          (values x y))))
+    (let-values (((x y) (if (and (struct? x) (struct? y))
+                            (values (struct->vector x) (struct->vector y))
+                            (values x y))))
+      (if (and (sequence? x) (sequence? y)
+               (= (sequence-length x)
+                  (sequence-length y)))
+          (for/and ([x-element x]
+                    [y-element y])
+            (syntax-equal? x-element y-element))
+          (equal? x y)))))
+
+(define-binary-check (check-syntax-equal? syntax-equal? actual expected))
+
