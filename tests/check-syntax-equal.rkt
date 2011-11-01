fc17f8a3310d6508fd056b49d4ebcec89e05fff1
Daniel King <danking@ccs.neu.edu>
2011-11-01 14:50:23 -0400

    Fix check-syntax-equal? to properly handle numbers and custom-hash-tables
    Apparently, hash? of a custom-hash-table is false, but dict? is
    true. Furthermore, numbers are sequences, but I'd rather not follow that rabbit
    hole in my equality procedure.

--- tests/check-syntax-equal.rkt
+++ tests/check-syntax-equal.rkt
@@ -10,14 +10,21 @@
     (let-values (((x y) (if (and (struct? x) (struct? y))
                             (values (struct->vector x) (struct->vector y))
                             (values x y))))
-      (cond [(and (hash? x) (hash? y)
-                  (= (sequence-length x)
-                     (sequence-length y)))
-             (for/and ([(x-key x-val) x]
-                       [(y-key y-val) y])
-                      (and (syntax-equal? x-key y-key)
-                           (syntax-equal? x-val y-val)))]
+      (cond [(and (dict? x) (dict? y)
+                  (not (list? x))
+                  (not (list? y))
+                  (= (sequence-length (in-dict x))
+                     (sequence-length (in-dict y))))
+             (for/and ([(x-key x-val) (in-dict x)])
+                      (let ((y-key (findf (lambda (k)
+                                            (syntax-equal? x-key k))
+                                          (dict-keys y))))
+                        (and y-key
+                             (syntax-equal? x-val
+                                            (dict-ref y y-key)))))]
             [(and (sequence? x) (sequence? y)
+                  (not (number? x))
+                  (not (number? y))
                   (= (sequence-length x)
                      (sequence-length y)))
              (for/and ([x-element x]
