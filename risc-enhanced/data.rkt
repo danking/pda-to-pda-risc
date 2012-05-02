08def84d3a7fe0f5e1789e2fc70d253e7afbecfc
Daniel King <danking@ccs.neu.edu>
2012-05-02 15:31:08 -0400

    decorate the tree with new data definitions
    The new data definitions enable me to record links between register defs and
    uses, label defs and uses, as well as live variables for every insn.

---
+++ risc-enhanced/data.rkt
@@ -0,0 +1,83 @@
+#lang racket
+(provide (all-defined-out))
+(require "../../racket-utils/mutable-set.rkt")
+
+(define (uninitialized-pda-term insn)
+  (pda-term (make-mutable-set) (make-mutable-set)
+            (make-mutable-set) (make-mutable-set)
+            insn))
+
+(struct pda-term (preds succs avail-regs live-regs insn)
+  #:transparent)
+
+(define (uninitialized-register name)
+  (register name #f #f (make-mutable-set)))
+
+(struct register (lexical-name
+                  uid
+                  [binding #:mutable]
+                  uses)
+  #:transparent)
+
+(define (uninitialized-label-name name)
+  (label-name name #f #f (make-mutable-set)))
+
+(struct label-name (lexical-name
+                    uid
+                    [binding #:mutable]
+                    uses)
+  #:transparent)
+
+(define (raise-to-term f)
+  (lambda (t . rest)
+    (match t
+      ((pda-term a b c d i)
+       (pda-term a b c d (apply f (cons i rest)))))))
+
+(define (raise-input-to-term f)
+  (lambda (t . rest)
+    (match t
+      ((pda-term a b c d i)
+       (apply f (cons i rest))))))
+
+;; A pda-term is
+;;
+;;   < preds, succs, avail-regs, live-regs, insn >
+;;
+;; preds ⊆ P(pda-term)
+;; succs ⊆ P(pda-term)
+;; avail-regs ⊆ P(registers)
+;; live-regs ⊆ P(registers)
+;; insn ⊆ Insn ∪ Insn*
+
+;; ** All four fields should be mutable sets.
+;;
+;; The preds field is a set of all instructions which could potentially *precede*
+;; this term in the dynamic execution of the PDA.
+;;
+;; The succs field is a set of all instructions which could potentially *succeed*
+;; this term in the dynamic execution of the PDA
+;;
+;; The avail-regs field is a set of all registers which are in-scope at this term
+;;
+;; The live-regs field is a set of all registers which could potentially be used by
+;; a term in the transitive closure of succs.
+
+
+;; A register is
+;;
+;;   < lexical-name, uid, binding, uses >
+;;
+;; lexical-name ⊆ String
+;; uid ⊆ Number
+;; binding ⊆ pda-term
+;; uses ⊆ P(pda-term)
+;;
+;; The lexical-name field is a Syntax object from the input text
+;;
+;; The uid field is a number which is unique amongst all registers in this PDA
+;; term.
+;;
+;; The binding field is the pda-term which binds this register
+;;
+;; The uses field is a set of pda-terms which reference this register
---
+++ risc-enhanced/from-risc.rkt
@@ -0,0 +1,77 @@
+#lang racket
+(require "../pdarisc-data.rkt"
+         (prefix-in enh: "data.rkt"))
+(provide convert/pdarisc)
+
+(define (convert/pdarisc pr)
+  (match pr
+    ((pdarisc insn-seq*)
+     (pdarisc (convert/insn-seq* insn-seq*)))))
+
+(define (convert/insn i)
+  (enh:uninitialized-pda-term
+   (match i
+     ((assign id val) (assign (convert/register id) (convert/rhs val)))
+     ((sem-act name params retvars action)
+      (sem-act name
+               (map (lambda (reg-list)
+                      (map convert/register reg-list))
+                    params)
+               (map (lambda (maybe-r)
+                      (if maybe-r
+                          (convert/register maybe-r)
+                          maybe-r))
+                    retvars)
+               action))
+     ((block insns) (block (convert/insn-seq insns)))
+     ((push rhs) (push (convert/rhs rhs)))
+     (_ i))))
+
+(define (convert/insn* i)
+  (enh:uninitialized-pda-term
+   (match i
+     ((label ids st tt param-lists bodies body)
+      (label (map convert/label-name ids)
+             st tt
+             (map (lambda (param-list)
+                    (map convert/register param-list))
+                  param-lists)
+             (map convert/insn-seq* bodies)
+             (convert/insn-seq* body)))
+     ((accept regs)
+      (accept (map convert/register regs)))
+     ((if-eos cnsq altr)
+      (if-eos (convert/insn* cnsq)
+              (convert/insn* altr)))
+     ((state-case r l cnsqs)
+      (state-case (convert/register r)
+                  l
+                  (map convert/insn-seq* cnsqs)))
+     ((token-case l cnsqs)
+      (token-case l (map convert/insn-seq* cnsqs)))
+     ((go target args)
+      (go (convert/label-name target)
+          (map convert/rhs args))))))
+
+(define (convert/insn-seq seq)
+  (map convert/insn seq))
+
+(define (convert/insn-seq* seq)
+  (match seq
+    ((list i ... i*)
+     (append (convert/insn-seq i)
+             (list (convert/insn* i*))))))
+
+(define (convert/label-name lbl)
+  (let ((name (label-name->symbol lbl)))
+    (enh:uninitialized-label-name name)))
+
+(define (convert/rhs rhs)
+  (match rhs
+    ((register) (convert/register rhs))
+    (_ rhs)))
+
+(define (convert/register reg)
+  (match reg
+    ((named-reg id) (enh:uninitialized-register id))
+    ((nameless-reg) (enh:uninitialized-register '_))))
