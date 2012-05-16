6daf12bb7bf8d9a13f4b967fbc50bcc0711395bf
Daniel King <danking@ccs.neu.edu>
2012-05-16 18:16:08 -0400

    fix cycles by putting traverse and unparse into data.rkt

--- risc-enhanced/data.rkt
+++ risc-enhanced/data.rkt
@@ -59,8 +59,7 @@
                              label-polynym?
                              label-polynym-extra-id
 
-                             label-name->symbol))
-         "unparse.rkt")
+                             label-name->symbol)))
 (provide (all-defined-out)
          ;; Re-exports
          (all-from-out "../pdarisc-data.rkt"))
@@ -178,44 +177,211 @@
       ((pda-term a b c d i)
        (apply f (cons i rest))))))
 
-;; A pda-term is
-;;
-;;   < preds, succs, avail-regs, live-regs, insn >
-;;
-;; preds ⊆ P(pda-term)
-;; succs ⊆ P(pda-term)
-;; avail-regs ⊆ P(registers)
-;; live-regs ⊆ P(registers)
-;; insn ⊆ Insn ∪ Insn*
 
-;; ** All four fields should be mutable sets.
-;;
-;; The preds field is a set of all instructions which could potentially *precede*
-;; this term in the dynamic execution of the PDA.
-;;
-;; The succs field is a set of all instructions which could potentially *succeed*
-;; this term in the dynamic execution of the PDA
-;;
-;; The avail-regs field is a set of all registers which are in-scope at this term
-;;
-;; The live-regs field is a set of all registers which could potentially be used by
-;; a term in the transitive closure of succs.
+;; General Fold
+(define (id x) x)
+(define (maybe-f f) (lambda (x) (if x (f x) x)))
+
+(define (traverse-pdarisc #:reguse [reguse* id]
+                          #:regdef [regdef* id]
+                          #:reg [reg* id]
+                          #:lbluse [lbluse* id]
+                          #:lbldef [lbldef* id]
+                          #:lbl [lbl* id]
+                          #:rhs [rhs* id]
+                          #:token [token* id]
+                          #:syntax [syntax* id]
+                          #:insn [touch-insn id]
+                          #:insn* [touch-insn* id]
+                          #:term [touch-term id]
+                          #:term* [touch-term* id]
+                          #:seq-cons [seq-cons cons]
+                          #:seq-end  [seq-end  empty]
+                          #:pdarisc [touch-pdarisc id])
+  (define syntax syntax*)
+  (define reg reg*)
+  (define reguse (compose reg reguse*))
+  (define regdef (compose reg regdef*))
+  (define lbl lbl*)
+  (define lbluse (compose lbl lbluse*))
+  (define lbldef (compose lbl lbldef*))
+  (define (rhs r)
+    (rhs* (match r
+            ((state id) (state (syntax id)))
+            ((register _ _ _ _) (reguse r))
+            (other other))))
+  (define token (compose syntax token*))
 
 
-;; A register is
-;;
-;;   < lexical-name, uid, binding, uses >
-;;
-;; lexical-name ⊆ Symbol
-;; uid ⊆ Number
-;; binding ⊆ pda-term
-;; uses ⊆ P(pda-term)
-;;
-;; The lexical-name field is a Syntax object from the input text
-;;
-;; The uid field is a number which is unique amongst all registers in this PDA
-;; term.
-;;
-;; The binding field is the pda-term which binds this register
-;;
-;; The uses field is a set of pda-terms which reference this register
+  (define (map/term-seq* seq)
+    (cond [(empty? seq) (error 'term-seq* "cannot have an empty term-seq*")]
+          [(empty? (rest seq)) (seq-cons (map/term* (first seq)) seq-end)]
+          [else (seq-cons (map/term (first seq)) (map/term-seq* (rest seq)))]))
+  (define (map/term-seq seq)
+    (foldr (lambda (x y)
+             (seq-cons (map/term x) y))
+           seq-end
+           seq))
+  (define (map/insn i)
+    (touch-insn
+     (match i
+       ((assign id val)
+        (assign (regdef id)
+                (rhs val)))
+       ((push val)
+        (push (rhs val)))
+       ((sem-act name params retvars action)
+        (sem-act (syntax name)
+                 (map reguse params)
+                 (map (lambda (rn)
+                        (if rn
+                            (regdef rn)
+                            rn))
+                      retvars)
+                 action))
+       ((drop-token) i)
+       ((get-token) i)
+       ((stack-ensure hdrm) (stack-ensure hdrm))
+       ((join-point label args)
+        (join-point (lbluse label) (map regdef args)))
+       ((block seq) (block (map/term-seq seq))))))
+  (define (map/term t)
+    (match t
+      ((pda-term a b c d i)
+       (touch-term (pda-term a b c d (map/insn i))))))
+  (define (map/insn* i)
+    (touch-insn*
+     (match i
+       ((label ids stack-types token-types
+               param-lists bodies body)
+        (label (map lbldef ids)
+               stack-types
+               token-types
+               (map (lambda (param-list)
+                      (map regdef param-list))
+                    param-lists)
+               (map map/term-seq* bodies)
+               (map/term-seq* body)))
+       ((block* seq)
+        (block* (map/term-seq* seq)))
+       ((accept vals)
+        (accept (map reguse vals)))
+       ((reject) i)
+       ((if-eos cnsq altr)
+        (if-eos (map/term* cnsq)
+                (map/term* altr)))
+       ((state-case st lookaheads cnsqs)
+        (state-case (reguse st)
+                    (map rhs lookaheads)
+                    (map map/term-seq* cnsqs)))
+       ((token-case lookaheads cnsqs)
+        (token-case (map (maybe-f token) lookaheads)
+                    (map map/term-seq* cnsqs)))
+       ((go target args)
+        (go (lbluse target)
+            (map rhs args))))))
+  (define (map/term* t)
+    (match t
+      ((pda-term a b c d i)
+       (touch-term (pda-term a b c d (map/insn* i))))))
+  (values
+   (lambda (pr)
+     (match pr
+       ((pdarisc seq) (touch-pdarisc (pdarisc (map/term-seq* seq))))))
+   map/term
+   map/term*))
+
+;; printer
+
+(define (unparse-rhs r)
+  (match r
+    ((pop) `(pop))
+    ((state id)
+     `(state ,id))
+    ((nterm id)
+     `(nterm ,id))
+    ((curr-token #f)
+     '(current-token))
+    ((curr-token n)
+     `(current-token ,n))
+    (_ r)))
+
+(define (unparse-insn i)
+  (match i
+    ((assign id val)
+     `(:= ,id ,val))
+    ((push val)
+     `(push ,val))
+    ((sem-act name params retvars action)
+     `(semantic-action ,name
+                       ,params
+                       ,retvars
+                       ,action))
+    ((drop-token)
+     'drop-token)
+    ((get-token)
+     'get-token)
+    ((stack-ensure hdrm)
+     `(stack-ensure ,hdrm))
+    ((join-point label args)
+     `(join-point ,label . ,args))
+    ((block insns)
+     `(block . ,insns))))
+
+(define (unparse-insn* i)
+  (match i
+   ((label ids stack-types token-types param-lists rhses body)
+    `(label ,(unparse-label-clauses ids stack-types token-types param-lists rhses)
+            . ,body))
+   ((block* insns)
+    `(block . ,insns))
+   ((accept vars)
+    `(accept . ,vars))
+   ((reject)
+    `(reject))
+   ((if-eos cnsq altr)
+    `(if-eos ,cnsq ,altr))
+   ((state-case var looks cnsqs)
+    `(state-case ,var
+                 . ,(map (lambda (look cnsq)
+                           (cons look cnsq))
+                         looks
+                         cnsqs)))
+   ((token-case looks cnsqs)
+    `(token-case . ,(map (lambda (l c)
+                           (cons (if l l #f) c))
+                         looks
+                         cnsqs)))
+   ((go target args)
+    `(go ,target . ,args))))
+
+(define (unparse-label-clauses ids stack-types token-types param-lists rhses)
+  (map (lambda (id stack-type token-type param rhs)
+         `(,id : ,stack-type
+               ,token-type
+               ,param
+               . ,rhs))
+       ids
+       stack-types
+       token-types
+       param-lists
+       rhses))
+
+(define (remove-struct:-prefix sym)
+  (string->symbol (substring (symbol->string sym) 7)))
+
+(define-values
+  (unparse unparse-term unparse-term*)
+  (traverse-pdarisc #:pdarisc (match-lambda ((pdarisc seq) seq))
+                    #:term pda-term-insn
+                    #:term* pda-term-insn
+                    #:insn unparse-insn
+                    #:insn* unparse-insn*
+                    #:rhs unparse-rhs
+                    #:syntax syntax-e
+                    #:lbl (lambda (l)
+                            `(label-name ,(label-name-uid l)
+                                         ,(label-name-lexical-name l)))
+                    #:reg (lambda (r)
+                            `(register ,(register-uid r)
+                                       ,(register-lexical-name r)))))
