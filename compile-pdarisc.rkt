c9a6ee0c6b06bceaa4aca2405726c515e8e48381
Nicholas Labich <labichn@gmail.com>
2013-02-14 19:42:10 -0500

    Finally pushing pdarisc->racket compiler.

--- compile-pdarisc.rkt
+++ compile-pdarisc.rkt
@@ -1,187 +1,206 @@
 #lang racket
+
 (require "pdarisc-data.rkt"
          "symbol-append.rkt"
          (for-template racket))
 (provide compile-pdarisc)
 
-;; compile-insn-seq* : PDA-RISC
-;;                     [Syntax [Lambda (Token -> Symbol)]]
-;;                     [Syntax [Lambda (Input-Stream -> Token)]]
-;;                     [Syntax [Lambda (Input-Stream -> Input-Stream)]]
-;;                     ->
-;;                     [Syntax [Lambda (Input-Stream -> Any)]]
+;; compile-pdarisc : PDA-RISC
+;;                   [Syntax [Lambda (Token -> Symbol)]]
+;;                   [Syntax [Lambda (Input-Stream -> Token)]]
+;;                   [Syntax [Lambda (Input-Stream -> Input-Stream)]]
+;;                   [Syntax [Lambda (Input-Stream -> Boolean)]]
+;;                   ->
+;;                   [Syntax [Lambda (Input-Stream -> Any)]]
 (define (compile-pdarisc p token-convert get-token drop-token eos-stream?)
-  (match p
-    ((pdarisc _ insns)
-     #`(lambda (input-stream)
-         (define-struct nterm (id) #:transparent)
-         (define-struct state (id) #:transparent)
-         (#,(compile-insn-seq* insns
-                               token-convert
-                               get-token
-                               drop-token
-                               eos-stream?)
-          input-stream
-          #f
-          (hasheq)
-          '())))))
-
-;; compile-pure-rhs : Pure-Rhs
-;;                    [Syntax Identifier]
-;;                    [Syntax Identifier]
-;;                    ->
-;;                    [Syntax Expression]
-;; tr and regs are the corresponding symbols to the token-register and
-;; register-environment identifiers in this context
-(define (compile-pure-rhs r tr regs)
-  (match r
-    ((state id) #`(make-state '#,id))
-    ((nterm id) #`(make-nterm '#,id))
-    ((curr-token #f) tr)
-    ((register) (compile-register-ref r regs))))
-
-;; compile-reg : Register -> [Syntax Identifier]
-(define (compile-register-name r)
-  (match r
-    ((named-reg id) id)
-    ((nameless-reg) #'_)))
-
-;; compile-reg-ref : Register [Syntax Identifier] -> Syntax
-(define (compile-register-ref r regs)
-  #`(dict-ref #,regs '#,(compile-register-name r)))
+  #`(lambda (I)
+      (define stk '())
+      (define TR #f)
+      (struct state (id))
+      (struct nterm (id))
+      #,(match p
+          ((pdarisc _ insns)
+           (compile-insn-seq* insns
+                              token-convert
+                              get-token
+                              drop-token
+                              eos-stream?)))))
 
 ;; compile-insn-seq* : Insn-Seq*
 ;;                     [Syntax [Lambda (Token -> Symbol)]]
 ;;                     [Syntax [Lambda (Input-Stream -> Token)]]
 ;;                     [Syntax [Lambda (Input-Stream -> Input-Stream)]]
-;;                     -> [Syntax [Lambda ([ListOf Token]
-;;                                         Token
-;;                                         [Dict Symbol Value]
-;;                                         [ListOf Value]
-;;                                         ->
-;;                                         Any)]]
+;;                     [Syntax [Lambda (Input-Stream -> Boolean)]]
+;;                     -> Syntax
 ;; produces the continuation representing this Insn-Seq*
 (define (compile-insn-seq* insns
                            token-convert
                            stream-get-token
                            stream-drop-token
                            eos-stream?)
-  (define (c insns)
-    (compile-insn-seq* insns
+  (match insns
+    ((list (assign _ reg var-rhs)
+           etc ...)
+     #`(let ((#,(compile-register reg)
+              #,(compile-var-rhs var-rhs)))
+         #,(compile-insn-seq* etc 
+                              token-convert
+                              stream-get-token
+                              stream-drop-token
+                              eos-stream?)))
+    ((list (push _ pure-rhs)
+           etc ...)
+     #`(begin (set! stk (cons #,(compile-pure-rhs pure-rhs) stk))
+              #,(compile-insn-seq* etc
+                                   token-convert
+                                   stream-get-token
+                                   stream-drop-token
+                                   eos-stream?)))
+    ((list (sem-act _ name in-regs out-regs act)
+           etc ...)
+     #`(let-values ((#,(map compile-maybe-var out-regs) #,(compile-foreign act in-regs)))
+         #,(compile-insn-seq* etc 
+                              token-convert
+                              stream-get-token
+                              stream-drop-token
+                              eos-stream?)))
+    ((list (drop-token _)
+           etc ...)
+     #`(begin (set! TR #f) #,(compile-insn-seq* etc
+                                                token-convert
+                                                stream-get-token
+                                                stream-drop-token
+                                                eos-stream?)))
+    ((list (get-token _)
+           etc ...)
+     #`(begin (set! TR (#,stream-get-token I))
+              (set! I (#,stream-drop-token I))
+              #,(compile-insn-seq* etc
+                                   token-convert
+                                   stream-get-token
+                                   stream-drop-token
+                                   eos-stream?)))
+    ((list (block _ insns)
+           etc ...)
+     (compile-insn-seq* (append insns etc)  
+                        token-convert
+                        stream-get-token
+                        stream-drop-token
+                        eos-stream?))
+    (else (compile-insn* (car insns) 
+                         token-convert
+                         stream-get-token
+                         stream-drop-token
+                         eos-stream?))))
+
+(define (compile-labeled-definition name
+                                    args
+                                    body 
+                                    token-convert
+                                    stream-get-token
+                                    stream-drop-token
+                                    eos-stream?)
+  #`(#,(compile-label-use name)
+     (lambda #,(map compile-register args) #,(compile-insn-seq* body 
+                                                           token-convert
+                                                           stream-get-token
+                                                           stream-drop-token
+                                                           eos-stream?))))
+
+(define (compile-label-use label)
+  (datum->syntax #'a (symbol-append 'l- (label-name-id label))))
+
+(define (compile-foreign a in-regs)
+  #`((lambda #,(map (curry compile-register-no-rename a) in-regs) #,a)
+     #,@(map compile-register in-regs)))
+
+(define (compile-insn* insn*
                        token-convert
                        stream-get-token
                        stream-drop-token
-                       eos-stream?))
-  (define cp compile-pure-rhs)
-  (define creg compile-register-name)
-  (define cref compile-register-ref)
+                       eos-stream?)
+  (match insn*
+    ((label _ names stacks token-regs arg-lists bodies label-body)
+     #`(letrec #,(map (lambda (name arg-list body)
+                        (compile-labeled-definition name
+                                                    arg-list
+                                                    body 
+                                                    token-convert
+                                                    stream-get-token
+                                                    stream-drop-token
+                                                    eos-stream?))
+                      names arg-lists bodies)
+         #,(compile-insn-seq* label-body 
+                              token-convert
+                              stream-get-token
+                              stream-drop-token
+                              eos-stream?)))
+    ((block* _ insns)
+     (compile-insn-seq* insns 
+                        token-convert
+                        stream-get-token
+                        stream-drop-token
+                        eos-stream?))
+    ((if-eos _ cnsq altr)
+     #`(if (#,eos-stream? I)
+           #,(compile-insn* cnsq 
+                            token-convert
+                            stream-get-token
+                            stream-drop-token
+                            eos-stream?)
+           #,(compile-insn* altr 
+                            token-convert
+                            stream-get-token
+                            stream-drop-token
+                            eos-stream?)))
+    ((state-case _ reg gaurds cnsqs)
+     #`(case (state-id #,(compile-register reg))
+         .
+         #,(map (lambda (gaurd cnsq)
+                  #`((#,(state-id gaurd)) #,(compile-insn-seq* cnsq 
+                                                               token-convert
+                                                               stream-get-token
+                                                               stream-drop-token
+                                                               eos-stream?)))
+                gaurds
+                cnsqs)))
+    ((token-case _ gaurds cnsqs)
+     #`(case (#,token-convert TR)
+         .
+         #,(map (lambda (gaurd cnsq)
+                  #`(#,(if gaurd (list gaurd) #'else)
+                     #,(compile-insn-seq* cnsq 
+                                          token-convert
+                                          stream-get-token
+                                          stream-drop-token
+                                          eos-stream?)))
+                gaurds
+                cnsqs)))
+    ((go _ label args)
+     #`(#,(compile-label-use label) #,@(map compile-pure-rhs args)))
+    ((accept _ regs)
+     #`(values #,@(map compile-register regs)))
+    ((reject _)
+     #`(error 'parser "Invalid input string."))))
 
-  (match insns
-    ((list (assign _ reg (pop))
-           etc ...)
-     #`(lambda (in tr regs stack)
-         (#,(c etc)
-          in
-          tr
-          (dict-set regs '#,(creg reg) (car stack))
-          (cdr stack))))
-    ((list (assign _ reg pure-rhs)
-           etc ...)
-     #`(lambda (in tr regs stack)
-         (#,(c etc) in tr
-          (dict-set regs '#,(creg reg) #,(cp pure-rhs #'tr #'regs))
-          stack)))
-    ((list (push _ pure-rhs)
-           etc ...)
-     #`(lambda (in tr regs stack)
-         (#,(c etc) in tr regs (cons #,(cp pure-rhs #'tr #'regs) stack))))
-    ((list (sem-act _ name in-regs out-regs act)
-           etc ...)
-     #`(lambda (in tr regs stack)
-         (#,(c etc) in tr
-          (dict-set regs '#,(creg (first out-regs))
-                    ((lambda #,(map creg in-regs)
-                       #,act)
-                     . #,(map (lambda (x) (cref x #'regs)) in-regs)))
-          stack)))
-    ((list (drop-token _)
-           etc ...)
-     #`(lambda (in tr regs stack)
-         (#,(c etc) in #f regs stack)))
-    ((list (get-token _)
-           etc ...)
-     #`(lambda (in tr regs stack)
-         (#,(c etc)
-          (#,stream-drop-token in)
-          (#,stream-get-token in)
-          regs stack)))
-    ((list (block _ insns)
-           etc ...)
-     (c (append insns etc)))
-    ((list (label _ names stacks token-regs arg-lists bodies label-body))
-     #`(lambda (in tr regs stack)
-         (letrec #,(map (lambda (name arg-list body)
-                          (compile-cond-clause name arg-list body c))
-                        names arg-lists bodies)
-           (#,(c label-body) in tr regs stack))))
-    ((list (block* _ insns))
-     (c insns))
-    ((list (if-eos _ cnsq altr))
-     #`(lambda (in tr regs stack)
-         (if (#,eos-stream? in)
-             (#,(c (list cnsq)) in tr regs stack)
-             (#,(c (list altr)) in tr regs stack))))
-    ((list (state-case _ reg gaurds cnsqs))
-     #`(lambda (in tr regs stack)
-         (case (state-id #,(cref reg #'regs))
-           .
-           #,(map (lambda (gaurd cnsq)
-                    #`((#,(state-id gaurd))
-                       (#,(c cnsq) in tr regs stack)))
-                  gaurds
-                  cnsqs))))
-    ((list (token-case _ gaurds cnsqs))
-     #`(lambda (in tr regs stack)
-         (case (#,token-convert tr)
-           .
-           #,(map (lambda (gaurd cnsq)
-                    #`(#,(if gaurd
-                             (list gaurd)
-                             #'else)
-                       (#,(c cnsq) in tr regs stack)))
-                  gaurds
-                  cnsqs))))
-    ((list (go _ label args))
-     #`(lambda (in tr regs stack)
-         ((#,(compile-label-name label) #,@(map (lambda (x)
-                                                  (cref x #'regs))
-                                                args))
-          in tr regs stack)))
-    ((list (accept _ regs))
-     #`(lambda (in tr regs stack)
-         #,(cons #'list (map (lambda (x) (cref x #'regs)) regs))))
-    ((list (reject _))
-     #`(lambda (in tr regs stack)
-         (format "no match in: ~a tr: ~a regs: ~a stack: ~a"
-                 in tr regs stack)))))
-
-(define (compile-label-name label)
-  (match label
-    ((label-name id) id)))
-
-(define (compile-cond-clause name arg-registers body compile-cnsq)
-  (define (make-dict-set key val regs)
-    #`(dict-set #,regs '#,(compile-register-name key) #,val))
-
-  (let ((lambda-args (map (compose gensym compile-register-name)
-                          arg-registers)))
-    #`(#,(compile-label-name name) (lambda #,lambda-args
-                                     (lambda (in tr regs stack)
-                                       (#,(compile-cnsq body)
-                                        in
-                                        tr
-                                        #,(foldl make-dict-set
-                                                 #'regs
-                                                 arg-registers
-                                                 lambda-args)
-                                        stack))))))
+(define (compile-maybe-var mv)
+  (if mv (compile-register mv) #'__))
+(define (compile-var-rhs rhs)
+  (if (pop? rhs)
+      #`(begin0 (car stk) (set! stk (cdr stk)))
+      (compile-pure-rhs rhs)))
+(define (compile-pure-rhs rhs)
+  (match rhs
+    ((register) (compile-register rhs))
+    ((state id) #`(state '#,id))
+    ((nterm id) #`(nterm '#,id))
+    ((curr-token n) #`TR)))
+(define (compile-register r)
+  (match r
+    ((named-reg name) (datum->syntax #'a (symbol-append 'r- name)))
+    ((nameless-reg) #'__)))
+;; act is there for syntax location.
+(define (compile-register-no-rename act r)
+  (match r
+    ((named-reg name) (datum->syntax act name))
+    ((nameless-reg) #'__)))
