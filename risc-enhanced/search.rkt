9f67bfb995c33c0c4e3dabe876479d8fbfa12df5
Daniel King <danking@ccs.neu.edu>
2013-05-17 16:26:10 -0400

    Provide both a forward and backward folding search

--- risc-enhanced/search.rkt
+++ risc-enhanced/search.rkt
@@ -2,17 +2,25 @@
 
 (require "../../racket-utils/workset-loop.rkt"
          "data.rkt")
-(provide folding-search)
+(provide folding-forward-search
+         folding-backwards-search)
 
-;; folding-search : [Term X -> X] X Term -> X
-(define (folding-search f b t)
+;; folding-forward-search : [Term X -> X] X Term -> X
+(define (folding-forward-search f b t)
+  (folding-search f b t pda-term-succs))
+
+;; folding-backwards-search : [Term X -> X] X Term -> X
+(define (folding-backwards-search f b t)
+  (folding-search f b t pda-term-preds))
+
+(define (folding-search f b t next-terms-to-look-at)
   (let loop ((W (seteq t))
              (seen (seteq))
              (b b))
     (if (set-empty? W) b
         (let ((t (set-first W))
               (W (set-rest W)))
-          (let* ((new-succs (set-subtract (pda-term-succs t) seen))
+          (let* ((new-succs (set-subtract (next-terms-to-look-at t) seen))
                  (new-W (set-union new-succs W))
                  (new-seen (set-union new-succs seen)))
             (loop new-W new-seen (f t b)))))))
