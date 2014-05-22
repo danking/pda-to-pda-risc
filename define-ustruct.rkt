0e8afb31440d6c19a3f57f1d4d5a5ad7b1d9f307
Daniel King <daniel.zidan.king@gmail.com>
2014-05-22 08:31:22 -0400

    Remove depedence on unstable/syntax

--- define-ustruct.rkt
+++ define-ustruct.rkt
@@ -1,7 +1,6 @@
 #lang racket
 (require (rename-in racket
-                    (define-struct define-struct*))
-         (for-syntax unstable/syntax))
+                    (define-struct define-struct*)))
 (provide define-ustruct)
 
 ;; (define-syntax symbol-append
@@ -28,11 +27,11 @@
   (syntax-case stx ()
     ((_ (name super) fields kws ...)
      #`(begin (define-struct (name super) fields #:transparent kws ...)
-              #,@(syntax-map (lambda (f)
-                               (make-updater-proc #'name f))
-                             #'fields)))
+              #,@(map (lambda (f)
+                        (make-updater-proc #'name f))
+                      (syntax->list #'fields))))
     ((_ name fields kws ...)
      #`(begin (define-struct name fields #:transparent kws ...)
-              #,@(syntax-map (lambda (f)
-                               (make-updater-proc #'name f))
-                             #'fields)))))
+              #,@(map (lambda (f)
+                        (make-updater-proc #'name f))
+                      (syntax->list #'fields))))))
