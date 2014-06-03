d3844bdd3003668d8f8bf414de18c0b4267600f7
Daniel King <daniel.zidan.king@gmail.com>
2014-06-03 15:16:03 -0400

    Simple algorithm to prioritize nodes based on an SCC toposort

---
+++ risc-enhanced/term-scc-toposort.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+(require graph)
+(provide pda-term-scc-priority-hash)
+
+;; pda-term-scc-priority-hash : PDA-RISC-ENH -> [Dict PDA-RISC-ENH Natural]
+(define (pda-term-scc-priority-hash t)
+  (let* ((sccs (scc t))
+         (sccs-graph (create-scc-graph sccs))
+         (ordered-sccs (tsort sccs-graph)))
+    ;; We use a mutable hash here because once we're done creating it, we never
+    ;; modify it and it *must* be fast. A priority queue is the fundamental data
+    ;; structure in our analysis, we're constantly removing and adding things to
+    ;; it.
+    (define priority-map (make-hash '()))
+
+    (for ([i (in-naturals 0)]
+          [scc ordered-sccs])
+      (for ([term scc])
+        (dict-set! priority-map term i)))
+
+    priority-map))
+
+(define (create-scc-graph sccs)
+  (define scc-graph (unweighted-graph/directed '()))
+
+  (for ([scc sccs])
+    (for ([term scc])
+      (for ([neighbor (in-neighbors term term)]
+            #:when (not (set-member? scc neighbor)))
+        (let ((neighbors-scc (findf (lambda (x) (set-member? x neighbor))
+                                    sccs)))
+          (unless neighbors-scc
+            (error 'create-scc-graph
+                   (format "The node ~a doesn't have an scc?!" neighbor)))
+          (add-edge! scc-graph scc neighbors-scc)))))
+
+  scc-graph)
